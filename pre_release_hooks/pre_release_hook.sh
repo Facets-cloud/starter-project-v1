387a477cfc54c1f0a279d15bf30f77ccbb6a8c93
pramodh-ayyappan <pramodh.ayyappan@facets.cloud>
2024-11-07 18:20:20 +0530

    pre release hook with exit code 2 added

---
+++ pre_release_hooks/pre_release_hook.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+echo "----------- These are echo statement from pre-release hook in infra-dev-sample-stack blueprint ----------------"
+echo "Resource Target Paths: $2"
+echo "---------------------------"
+exit 2
