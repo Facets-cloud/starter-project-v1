13c1a7804740fcd3ed442ae0867b4aedd585908e
samarthya-gupta1 <104773627+samarthya-gupta1@users.noreply.github.com>
2024-11-12 10:38:49 +0530

    Update pre_release_hook.sh

--- pre_release_hooks/pre_release_hook.sh
+++ pre_release_hooks/pre_release_hook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "----------- These are echo statement from pre-release hook in infra-dev-sample-stack blueprint ----------------"
-echo "Resource Target Paths: $2"
+echo "Resource Target Paths: $1"
 echo "---------------------------"
 exit 2
