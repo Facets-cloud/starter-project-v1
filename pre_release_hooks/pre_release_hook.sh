0ac98aaeec0ea02c6a7f7217cf8207a2b4e9d0e9
pramodh-ayyappan <pramodh.ayyappan@facets.cloud>
2024-11-12 16:40:40 +0530

    viewing the file

--- pre_release_hooks/pre_release_hook.sh
+++ pre_release_hooks/pre_release_hook.sh
@@ -2,5 +2,6 @@
 set -e
 echo "----------- These are echo statement from pre-release hook in infra-dev-sample-stack blueprint ----------------"
 echo "Resource Target Paths: $1"
+cat $1
 echo "---------------------------"
 exit 2
